f59ee89afd6024565d99e4fd5b93f1bdecc3d3bf
student1232354 <tarasromanenko33@gmail.com>
2026-06-23 15:06:40 +0300

    first update

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,33 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const rawobj = {};
+  const splittedobj = sourceString.split(';');
+
+  for (let i = 0; i < splittedobj.length; i++) {
+    const newItem = splittedobj[i].trim();
+
+    if (!newItem) {
+      continue;
+    }
+
+    const arraynew = newItem.split(':');
+
+    const keyofarray = arraynew[0];
+    const valueofarray = arraynew[1];
+
+    if (valueofarray === undefined) {
+      continue;
+    }
+
+    const preparedkey = keyofarray.trim();
+
+    const preparedvalue = valueofarray.trim().replace(/;$/, '');
+
+    rawobj[preparedkey] = preparedvalue;
+  }
+
+  return rawobj;
 }
 
 module.exports = convertToObject;
